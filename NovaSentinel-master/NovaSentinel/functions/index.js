a93a46bbc625dda59f8892eaad92dc790190914d
XxTheRios11xX <117194763+XxTheRios11xX@users.noreply.github.com>
2024-08-10 13:04:34 -0500

    Posible Envio Notificacion con Ubicacion
    Ojala esta sea, lptm

--- NovaSentinel-master/NovaSentinel/functions/index.js
+++ NovaSentinel-master/NovaSentinel/functions/index.js
@@ -5,19 +5,19 @@ admin.initializeApp();
 exports.sendNotification = functions.firestore.document('alerts/{alertId}')
     .onCreate((snap, context) => {
         const alert = snap.data();
-        const entityId = alert.entityId;
+        const entityId = alert.entityID; // Asegúrate de que sea 'entityID' como en la colección 'alerts'
 
-        return admin.firestore().collection('entities').doc(entityId).get()
+        return admin.firestore().collection('entidades').doc(entityId).get() // Cambiado a 'entidades'
             .then(doc => {
                 if (!doc.exists) {
                     console.log('No such document!');
                     return;
                 }
-                const token = doc.data().token;
+                const token = doc.data().fcmToken; // Cambiado a 'fcmToken'
 
                 const message = {
                     notification: {
-                        title: alert.title,
+                        title: alert.titulo, // Cambiado a 'titulo'
                         body: alert.body
                     },
                     token: token,
@@ -42,8 +42,8 @@ exports.updateToken = functions.https.onRequest((request, response) => {
     const token = request.body.token;
     const entityId = request.body.entityId;
 
-    admin.firestore().collection('entities').doc(entityId).update({
-        token: token
+    admin.firestore().collection('entidades').doc(entityId).update({
+        fcmToken: token // Cambiado a 'fcmToken'
     })
     .then(() => {
         response.send("Token updated successfully");
@@ -53,4 +53,3 @@ exports.updateToken = functions.https.onRequest((request, response) => {
         response.status(500).send(error);
     });
 });
-
