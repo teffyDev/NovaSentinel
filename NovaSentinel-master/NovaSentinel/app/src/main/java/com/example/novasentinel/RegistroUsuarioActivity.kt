d8036af848fb845539e94adfcf1232919f748912
Estafanny Ayala <117194683+teffyDev@users.noreply.github.com>
2024-08-22 11:53:06 -0500

    ojo 2

--- NovaSentinel-master/NovaSentinel/app/src/main/java/com/example/novasentinel/RegistroUsuarioActivity.kt
+++ NovaSentinel-master/NovaSentinel/app/src/main/java/com/example/novasentinel/RegistroUsuarioActivity.kt
@@ -52,35 +52,6 @@ class RegistroUsuarioActivity : AppCompatActivity() {
         txtTipoIdentificacionU = findViewById(R.id.txtTipoIdentificacionU)
         txtEntidadAsoc = findViewById(R.id.TexEntidadAsoc)
 
-        // Inicializar botones de mostrar/ocultar contraseña
-        btnShowHidePasswordU = findViewById(R.id.btnShowHidePasswordU)
-        btnShowHidePasswordUR = findViewById(R.id.btnShowHidePasswordUR)
-
-        var isPasswordVisibleU = false
-        var isPasswordVisibleUR = false
-
-        btnShowHidePasswordU.setOnClickListener {
-            isPasswordVisibleU = !isPasswordVisibleU
-            txtContraseñaU.inputType = if (isPasswordVisibleU) {
-                InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-            } else {
-                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-            }
-            txtContraseñaU.setSelection(txtContraseñaU.text.length)
-            btnShowHidePasswordU.setImageResource(if (isPasswordVisibleU) R.drawable.ojoan else R.drawable.ojocn)
-        }
-
-        btnShowHidePasswordUR.setOnClickListener {
-            isPasswordVisibleUR = !isPasswordVisibleUR
-            txtContraseñaUR.inputType = if (isPasswordVisibleUR) {
-                InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
-            } else {
-                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
-            }
-            txtContraseñaUR.setSelection(txtContraseñaUR.text.length)
-            btnShowHidePasswordUR.setImageResource(if (isPasswordVisibleUR) R.drawable.ojoan else R.drawable.ojocn)
-        }
-
         // Desactivar teclado para los campos de tipo de documento y entidad
         txtTipoIdentificacionU.inputType = InputType.TYPE_NULL
         txtEntidadAsoc.inputType = InputType.TYPE_NULL
@@ -116,6 +87,38 @@ class RegistroUsuarioActivity : AppCompatActivity() {
                 showEntityPickerDialog()
             }
         }
+
+
+
+        // Inicializar botones de mostrar/ocultar contraseña
+        btnShowHidePasswordU = findViewById(R.id.btnShowHidePasswordU)
+        btnShowHidePasswordUR = findViewById(R.id.btnShowHidePasswordUR)
+
+        var isPasswordVisibleU = false
+        var isPasswordVisibleUR = false
+
+        btnShowHidePasswordU.setOnClickListener {
+            isPasswordVisibleU = !isPasswordVisibleU
+            txtContraseñaU.inputType = if (isPasswordVisibleU) {
+                InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            } else {
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+            txtContraseñaU.setSelection(txtContraseñaU.text.length)
+            btnShowHidePasswordU.setImageResource(if (isPasswordVisibleU) R.drawable.ojocn else R.drawable.ojocn)
+        }
+
+        btnShowHidePasswordUR.setOnClickListener {
+            isPasswordVisibleUR = !isPasswordVisibleUR
+            txtContraseñaUR.inputType = if (isPasswordVisibleUR) {
+                InputType.TYPE_TEXT_VARIATION_VISIBLE_PASSWORD
+            } else {
+                InputType.TYPE_CLASS_TEXT or InputType.TYPE_TEXT_VARIATION_PASSWORD
+            }
+            txtContraseñaUR.setSelection(txtContraseñaUR.text.length)
+            btnShowHidePasswordUR.setImageResource(if (isPasswordVisibleUR) R.drawable.ojoan else R.drawable.ojocn)
+        }
+
 
         // Asegurarse de que el teclado no aparezca
         txtFechaU.setOnFocusChangeListener { v, hasFocus ->
@@ -201,47 +204,46 @@ class RegistroUsuarioActivity : AppCompatActivity() {
         db.collection("entidades")
             .get()
             .addOnSuccessListener { result ->
-                val entities = result.map { it.getString("nombre") ?: "" }
+                val entities = result.map { it.getString("nombreEmpresa") ?: "Sin nombre" }
                 val builder = AlertDialog.Builder(this)
-                builder.setTitle("Selecciona una entidad")
+                builder.setTitle("Selecciona tu entidad")
                 builder.setItems(entities.toTypedArray()) { _, which ->
                     txtEntidadAsoc.setText(entities[which])
                 }
                 builder.show()
             }
             .addOnFailureListener { e ->
-                Toast.makeText(this, "Error al cargar las entidades: ${e.message}", Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Error al obtener entidades: ${e.message}", Toast.LENGTH_SHORT).show()
             }
     }
 
     private fun registrarUsuario() {
-        val nombre = txtNombreU.text.toString()
-        val identificacion = txtIdentificacionU.text.toString()
-        val correo = txtCorreoU.text.toString()
-        val fechaNacimiento = txtFechaU.text.toString()
-        val genero = txtGenero.text.toString()
-        val tipoIdentificacion = txtTipoIdentificacionU.text.toString()
-        val entidadAsociada = txtEntidadAsoc.text.toString()
-        val contraseña = txtContraseñaU.text.toString()
-        val contraseñaR = txtContraseñaUR.text.toString()
+        // Obtener los datos del usuario
+        val nombre = txtNombreU.text.toString().trim()
+        val identificacion = txtIdentificacionU.text.toString().trim()
+        val correo = txtCorreoU.text.toString().trim()
+        val fechaNacimiento = txtFechaU.text.toString().trim()
+        val genero = txtGenero.text.toString().trim()
+        val tipoIdentificacion = txtTipoIdentificacionU.text.toString().trim()
+        val entidadAsociada = txtEntidadAsoc.text.toString().trim()
+        val contraseña = txtContraseñaU.text.toString().trim()
+        val contraseñaRepetida = txtContraseñaUR.text.toString().trim()
 
-        if (nombre.isEmpty() || identificacion.isEmpty() || correo.isEmpty() || fechaNacimiento.isEmpty() ||
-            genero.isEmpty() || tipoIdentificacion.isEmpty() || entidadAsociada.isEmpty() ||
-            contraseña.isEmpty() || contraseñaR.isEmpty()) {
-            Toast.makeText(this, "Por favor, llena todos los campos.", Toast.LENGTH_SHORT).show()
+        // Verificar que las contraseñas coincidan
+        if (contraseña != contraseñaRepetida) {
+            Toast.makeText(this, "Las contraseñas no coinciden", Toast.LENGTH_SHORT).show()
             return
         }
 
-        if (contraseña != contraseñaR) {
-            Toast.makeText(this, "Las contraseñas no coinciden.", Toast.LENGTH_SHORT).show()
-            return
-        }
-
+        // Registrar al usuario en Firebase Auth y Firestore
         auth.createUserWithEmailAndPassword(correo, contraseña)
             .addOnCompleteListener(this) { task ->
                 if (task.isSuccessful) {
-                    val user = auth.currentUser
-                    val userData = hashMapOf(
+                    // Obtener el ID del usuario registrado
+                    val userId = auth.currentUser?.uid
+
+                    // Crear un mapa con los datos del usuario
+                    val usuario = hashMapOf(
                         "nombre" to nombre,
                         "identificacion" to identificacion,
                         "correo" to correo,
@@ -249,22 +251,22 @@ class RegistroUsuarioActivity : AppCompatActivity() {
                         "genero" to genero,
                         "tipoIdentificacion" to tipoIdentificacion,
                         "entidadAsociada" to entidadAsociada
+                        // Agrega más campos según sea necesario
                     )
 
-                    user?.let {
-                        db.collection("usuarios").document(it.uid)
-                            .set(userData)
-                            .addOnSuccessListener {
-                                Toast.makeText(this, "Registro exitoso", Toast.LENGTH_SHORT).show()
-                                val intent = Intent(this, InicioUsuarioActivity::class.java)
-                                startActivity(intent)
-                            }
-                            .addOnFailureListener { e ->
-                                Toast.makeText(this, "Error al guardar los datos: ${e.message}", Toast.LENGTH_SHORT).show()
-                            }
-                    }
+                    // Agregar el usuario a Cloud Firestore
+                    db.collection("usuarios")
+                        .document(userId!!)
+                        .set(usuario)
+                        .addOnSuccessListener {
+                            Toast.makeText(this, "Usuario registrado correctamente", Toast.LENGTH_SHORT).show()
+                            // Aquí puedes redirigir al usuario a otra actividad si deseas
+                        }
+                        .addOnFailureListener { e ->
+                            Toast.makeText(this, "Error al registrar usuario: $e", Toast.LENGTH_SHORT).show()
+                        }
                 } else {
-                    Toast.makeText(this, "Error en el registro: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                    Toast.makeText(this, "Error al registrar usuario: ${task.exception?.message}", Toast.LENGTH_SHORT).show()
                 }
             }
     }
